e1c9809a1dc25771adc58f73f3d300810c7566e1
michelle Parisi <parimm@farmingdale.edu>
2024-09-16 19:07:12 -0400

    check

--- src/main/java/org/example/csc311_hw3_groupbased/HelloController.java
+++ src/main/java/org/example/csc311_hw3_groupbased/HelloController.java
@@ -7,6 +7,8 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+    //this is to see if commits work for me
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
