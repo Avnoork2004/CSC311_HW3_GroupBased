a99a70ec8f0184f1e5c8d989ba1163d35c13c8d4
michelle Parisi <parimm@farmingdale.edu>
2024-09-17 10:26:45 -0400

    images are place in the project now

--- src/main/java/org/example/csc311_hw3_groupbased/HelloApplication.java
+++ src/main/java/org/example/csc311_hw3_groupbased/HelloApplication.java
@@ -1,6 +1,7 @@
 package org.example.csc311_hw3_groupbased;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -8,13 +9,22 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private Stage stage;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("maze1.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("maze!");
         stage.setScene(scene);
         stage.show();
+    }
+    public void Maze2(ActionEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("maze2.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("maze!");
+        stage.setScene(scene);
+        stage.show();
+
     }
 
 
--- src/main/java/org/example/csc311_hw3_groupbased/HelloController.java
+++ src/main/java/org/example/csc311_hw3_groupbased/HelloController.java
@@ -1,16 +1,60 @@
 package org.example.csc311_hw3_groupbased;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
-
-    //this is to see if commits work for me
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView Bot;
+
+
+   // NOTE: car might not be in the right spot, might change spot if needed
+    @FXML
+    private ImageView Car;
+
+    @FXML
+    private Button SwitchToMaze2Button;
+
+    @FXML
+    private Button SwitchToMaze1Button;
+
+    @FXML
+    private Button animateButton2;
+
+    @FXML
+    private Button animateButton;
+
+    @FXML
+    private ImageView maze1;
+
+
+
+    //this is to start the Animation(maze 1)
+
+    @FXML
+    void AnimationStart(ActionEvent event) {
+
     }
+    //this is to start the Animation(maze 2)
+
+    @FXML
+    void AnimationStart2(ActionEvent event) {
+
+    }
+
+    //this is to switch to Maze 2 from Maze 1
+
+
+    @FXML
+    void SwitchToMaze2(ActionEvent event) {
+
+    }
+    //this is to switch to Maze 1 from Maze 2
+
+    @FXML
+    void SwitchToMaze1(ActionEvent event) {}
+
 }
